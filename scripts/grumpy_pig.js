63c6cdff48dc5bf09d7efbba40ea94e3098b9bc8
Charan Raj Musali <cmusali@cmusali.remote.csb>
2022-12-10 18:43:35 +0530

    some more github workflow learning exercies

---
+++ scripts/grumpy_pig.js
@@ -0,0 +1,17 @@
+/*
+ * All this program does is print 
+ * "I am a lazy pig for 1000 times" 
+ */
+
+
+
+function lazy_pig(){
+	// Say i am a lazy pig here
+	for (let i = 1; i <= 1000; i++) {
+		console.log(String(i) + '|' + "I AM A GRUMPY 28 YEAR OLD PIG, OINK, OINK, OINK ...")
+	}
+}
+
+
+
+lazy_pig()
